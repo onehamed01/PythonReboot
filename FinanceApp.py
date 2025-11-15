5ece08545abe4965f236251ed45703d469965463
onehamed01 <onehamed01@Gmail.com>
2025-11-15 22:46:24 +0000

    first commit

---
+++ FinanceApp.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+import csv
+
+class FinanceAnalyzer:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def FetchData(self):
+        full_data = []
+        with open(self.filename, newline = '', encoding='utf-8') as f:
+            next(f) #ignore first line
+
+            csv_file = csv.reader(f)
+
+            for line in csv_file:
+                date, des, amount, cat = line
+
+                full_data.append((
+                    date.datetime.strptime(date, "%Y-%m-%d"),
+                    des,
+                    float(amount),
+                    cat
+                ))
+        
+        return full_data
+
+    def FilterByCategory(self, expenses, category):
+        return [cat[3] for cat in expenses if cat[3].lower() == category.lower()]
+
+    def NegativeExpenses(self, expenses):
+        return [exp for exp in expenses if exp[2] < 0]
